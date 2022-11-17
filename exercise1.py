ed82a4a727edcd30439c99c97c5c04016b5a348f
Eoinburke <burkeyeoin45@gmail.com>
2022-11-17 17:11:27 +0000

    Card game

---
+++ exercise1.py
@@ -0,0 +1,25 @@
+plr_one = [10, 6, 8, 9, 7, 12, 7]
+
+plr_two = [7, 6, 9, 5, 2, 8, 11]
+
+if 10 > 7:
+    print("Player 1 Wins!!")
+
+    if 6 == 6:
+        print("Draw!")
+
+        if 9 > 8:
+            print("Player 2 Win!")
+
+            if 7 > 2:
+                print("Player 1 Win!")
+
+                if 12 > 8:
+                    print("Player 1 win!")
+
+                    if 11 > 7:
+                        print("Player 2 win")
+                        print("Player 1 wins the game by 4 wins to 2")
+
+
+
